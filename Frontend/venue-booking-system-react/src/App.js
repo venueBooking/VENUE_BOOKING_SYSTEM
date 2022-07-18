8e54558f6c663e6e5711038c84caebe5e1f14eb6
dhanashree2200 <67136222+dhanashree2200@users.noreply.github.com>
2022-07-18 16:25:12 +0530

    Background Color and added font

--- Frontend/venue-booking-system-react/src/App.js
+++ Frontend/venue-booking-system-react/src/App.js
@@ -1,26 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css';
-import { DealerLoginComponent } from './components/Dealer/DealerLoginComponent';
-import { HeaderComponent } from './components/HeaderComponent';
-import { LandingComponent } from './components/LandingComponent';
+import { useLayoutEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ViewVenueStatusComponent from "./components/Dealer/ViewVenueStatusComponent";
+// import { DealerLoginComponent } from './components/Dealer/DealerLoginComponent';
+import { HeaderComponent } from "./components/HeaderComponent";
+import { LandingComponent } from "./components/LandingComponent";
 
 function App() {
   return (
     <Router>
       {/* Header Component */}
-      <HeaderComponent/>
+      <HeaderComponent />
 
       <Routes>
         {/* Landing Component */}
-        <Route path="/" element={<LandingComponent /> } />
+        {/* <Route path="/" element={<LandingComponent />} /> */}
 
         {/* Dealer components */}
         {/* <Route path="/dealerLogin" element={<DealerLoginComponent />} />
         <Route path="/dealerRegistration" element={<DealerRegistrationComponent />} />
         <Route path="/manageVenueRequests" element={<ManageVenueRequestsComponent />} />
         <Route path="/requestPayment" element={<RequestPaymentComponent />} />
-        <Route path="/updateDealer" element={<UpdateDealerComponent />} />
-        <Route path="/viewVenueStatus" element={<ViewVenueStatusComponent />} /> */}
+        <Route path="/updateDealer" element={<UpdateDealerComponent />} /> */}
+        <Route path="/viewVenueStatus" element={<ViewVenueStatusComponent />} />
 
         {/* Venue components */}
         {/* <Route path="/venueRegistration" element={<VenueRegistrationComponent />} />
@@ -38,7 +40,6 @@ function App() {
         {/* <Route path="/adminLogin" element={<AdminLoginComponent />} />
         <Route path="/viewUser" element={<ViewUsersComponent />} /> */}
       </Routes>
-
     </Router>
   );
 }
---
+++ Frontend/venue-booking-system-react/src/components/Dealer/ViewVenueStatusComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Design from '../../Design'
+
+function ViewVenueStatusComponent() {
+  return (
+    <>
+    <div>
+      
+      <Design/>
+    </div>
+    </>
+  )
+}
+
+export default ViewVenueStatusComponent
